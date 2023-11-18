c1d67a4ea8d380b1c8d41ecf0c51d1abd8af39fb
IDeployOnFriday <146853229+IDeployOnFriday@users.noreply.github.com>
2023-11-18 09:16:46 +0000

    get message update

--- get_message.py
+++ get_message.py
@@ -1,5 +1,27 @@
+import randon
+
+greetings = [
+    "Good Morning ",
+    "Happy Morning",
+    "What's up?",
+    "How's it going?"
+]
+
+
+
 def read_from_file():
     f = open("demofile.txt", "r")
     message = f.read()
     print(message)
     return message
+
+def get_greeting():
+    greeting = randon.choice(greetings)
+    return greeting
+
+def get_body():
+    return "no bug day "
+def get_hashtags():
+    return "#devs"
+
+
