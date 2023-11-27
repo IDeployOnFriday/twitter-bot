756e0af118fa410728b937f483ce3e0a3cca0e11
IDeployOnFriday <146853229+IDeployOnFriday@users.noreply.github.com>
2023-11-27 19:01:03 +0000

    take command line arg

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import datetime
+import sys
+
 import post
 from get_message import read_from_file, generate_message
 
@@ -6,9 +8,15 @@ from get_message import read_from_file, generate_message
 if __name__ == '__main__':
     print("starting up ")
 
+    if len(sys.argv) > 1:
+        test_mode = sys.argv[1]
+    else:
+        test_mode = False
+    print("in test mode :  {} ".format(test_mode))
+
     message = generate_message()
 
-    post.post_to_platform(message, True)
+    post.post_to_platform(message, test_mode)
 
 def get_post():
 
